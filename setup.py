bb30fe7f7d97d5380c8d61f483c88ca21a938629
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-09-01 15:44:04 +0200

    Fix python -m build

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     setup_requires=["cffi>=1.0.0"],
-    cffi_modules=["buildFFI.py:libutil", "buildFFI.py:libstore", "buildFFI.py:libexpr"],
-    install_requires=["cffi>=1.0.0"]
+    cffi_modules=["src/buildFFI.py:libutil", "src/buildFFI.py:libstore", "src/buildFFI.py:libexpr"],
+    install_requires=["cffi>=1.0.0"],
+    package_data={"python_nix": ["src/py.typed", "src/*.pyi"]},
     )
