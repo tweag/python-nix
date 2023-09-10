2ce3752f8880e7453b29504988f3c331dfe970ea
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-09-10 14:51:27 +0200

    examples/plugin_entry: wasmImport

--- examples/plugin_entry.py
+++ examples/plugin_entry.py
@@ -17,4 +17,40 @@ def pyImport(x):
 imp = nix.expr.PrimOp(pyImport)
 nix.expr.lib.nix_register_primop(imp._primop)
 
+
+
+def wrapAsm(fun, store):
+    t = fun.type(store)
+    arity = len(t.params)
+    def arg0(x):
+        return fun(store)
+    def arg1(x):
+        return fun(store, x.force())
+    def arg2(x, y):
+        return fun(store, x.force(), y.force())
+    def arg3(x, y, z):
+        return fun(store, x.force(), y.force(), z.force())
+    return [arg0, arg1, arg2, arg3][arity]
+
+
+def wasmImport(x):
+    from wasmtime import Store, Module, Instance, WasiConfig, Linker
+
+
+    store = Store()
+    store.set_wasi(WasiConfig())
+    linker = Linker(store.engine)
+    linker.define_wasi()
+
+    module = Module.from_file(store.engine, str(x))
+    instance = linker.instantiate(store, module)
+    ret = {}
+    for key, func in instance.exports(store)._extern_map.items():
+        if key != "memory":
+            ret[key] = wrapAsm(func, store)
+    return ret
+
+imp2 = nix.expr.PrimOp(wasmImport)
+nix.expr.lib.nix_register_primop(imp2._primop)
+
 print("Python plugin loaded!")
